eef853e6b21aad2996a8321809199e97c37d9301
Abdelrahman Alasal <AbdelrahmanAlasal@gmail.com>
2024-06-16 13:12:53 +0300

    Single Responsibility Principle

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using SingleResponsibilityPrinciple.SOLID;
+using System;
+using System.Diagnostics;
+
+var journals = new Journal();
+
+journals.AddEntry("Dummy Entry 1");
+journals.AddEntry("Dummy Entry 2");
+journals.AddEntry("Dummy Entry 3");
+
+System.Console.WriteLine(journals);
+
+var persist = new Persistence();
+var filePath = @"SingleResponsibilities\journal.txt";
+persist.SaveToFile(journals, filePath);
+Process.Start("notepad.exe", filePath);
---
+++ SingleResponsibilityPrinciple/Journal.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.SOLID
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new();
+        private static int count = 0;
+
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+    }
+}
